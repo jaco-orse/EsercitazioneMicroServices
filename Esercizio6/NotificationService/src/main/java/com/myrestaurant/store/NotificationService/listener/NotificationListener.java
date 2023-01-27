03bf73789403640d21be96e18bb76e9b439ec5c9
jaco-orse <jaco.orse97@gmail.com>
2023-01-27 12:50:02 +0100

    Email  e SMS Listener Implementation

--- Esercizio6/NotificationService/src/main/java/com/myrestaurant/store/NotificationService/listener/NotificationListener.java
+++ Esercizio6/NotificationService/src/main/java/com/myrestaurant/store/NotificationService/listener/NotificationListener.java
@@ -1,37 +1,7 @@
 package com.myrestaurant.store.NotificationService.listener;
 
-import com.myrestaurant.store.NotificationService.service.EmailService;
-import com.myrestaurant.store.NotificationService.service.SMSService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Service;
+public interface NotificationListener {
 
-import java.util.List;
+    public void onAddPizzasToRestaurant(String message);
 
-@Service
-@Slf4j // permette di configurare il logger
-@RequiredArgsConstructor
-public class NotificationListener {
-
-    private final EmailService emailService;
-    private final SMSService smsService;
-
-    /*
-    @RabbitListener(queues = {"${app.rabbitmq.pizzas-added-routingkey}"})
-    public void onAddPizzasToRestaurant(String event){
-        log.info("Into onAddPizzasToRestaurant method.");
-        emailService.sendMessage(event);
-        smsService.sendMessage(event);
-    }
-    */
-
-    @RabbitListener(queues = {"${app.rabbitmq.notify-pizzas-added-to-restaurant-routingkey}"})
-    public void onAddPizzasToRestaurant(String message){
-        log.info("Into onAddPizzasToRestaurant method.");
-        //String event = "Added n." + pizzas.size() + "pizzas.";
-        String event = message;
-        emailService.sendMessage(event);
-        smsService.sendMessage(event);
-    }
 }
